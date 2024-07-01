a6bb18e376dcd8d62c73f156ba8afdf6a326ebb2
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-01 16:41:12 +0300

    modified model.py

---
+++ server/models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy=True)
+
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(200), nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', lazy=True)
+
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant = db.relationship('Restaurant', backref=db.backref('restaurant_pizzas', cascade='all,delete'))
+    pizza = db.relationship('Pizza', backref=db.backref('restaurant_pizzas', cascade='all,delete'))

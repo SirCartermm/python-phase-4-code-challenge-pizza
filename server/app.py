59c15023e5a9f58311dba744928cfe424dda8282
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-02 18:56:12 +0300

    modified app.py

--- server/app.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from models import db, Restaurant, RestaurantPizza, Pizza
-from flask_migrate import Migrate # type: ignore
-from flask import Flask, request, make_response # type: ignore

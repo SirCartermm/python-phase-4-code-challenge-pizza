f4623d47c0f03c21c804d2e350d75d3bd6d4f3e4
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-02 19:53:04 +0300

    modified app.py

--- server/app.py
+++ server/app.py
@@ -1,13 +1,13 @@
-from flask import jsonify, request
-from server import app, db
+from flask import jsonify, request # type: ignore
+from server .import app, db # type: ignore
 from server.models import Restaurant, Pizza, RestaurantPizza
 
-@app.route('/restaurants', methods=['GET'])
+@app.route('/restaurants', methods=['GET']) # type: ignore
 def get_restaurants():
     restaurants = Restaurant.query.all()
     return jsonify([r.to_dict() for r in restaurants])
 
-@app.route('/restaurants/<int:id>', methods=['GET'])
+@app.route('/restaurants/<int:id>', methods=['GET']) # type: ignore
 def get_restaurant(id):
     restaurant = Restaurant.query.get(id)
     if restaurant:
@@ -15,7 +15,7 @@ def get_restaurant(id):
     else:
         return jsonify({'error': 'Restaurant not found'}), 404
 
-@app.route('/restaurants/<int:id>', methods=['DELETE'])
+@app.route('/restaurants/<int:id>', methods=['DELETE']) # type: ignore
 def delete_restaurant(id):
     restaurant = Restaurant.query.get(id)
     if restaurant:
@@ -25,19 +25,19 @@ def delete_restaurant(id):
     else:
         return jsonify({'error': 'Restaurant not found'}), 404
 
-@app.route('/pizzas', methods=['GET'])
+@app.route('/pizzas', methods=['GET']) # type: ignore
 def get_pizzas():
     pizzas = Pizza.query.all()
     return jsonify([p.to_dict() for p in pizzas])
 
-@app.route('/restaurant_pizzas', methods=['POST'])
+@app.route('/restaurant_pizzas', methods=['POST']) # type: ignore
 def create_restaurant_pizza():
     data = request.get_json()
     pizza_id = data.get('pizza_id')
     restaurant_id = data.get('restaurant_id')
     price = data.get('price')
     if pizza_id and restaurant_id and price:
-        restaurant_pizza = RestaurantPizza(restaurant_id=restaurant_id, pizza_id=pizza_id, price=price)
+        restaurant_pizza = RestaurantPizza(restaurant_id=restaurant_id, pizza_id=pizza_id, price=price) # type: ignore
         db.session.add(restaurant_pizza)
         db.session.commit()
         return jsonify(restaurant_pizza.to_dict()), 201

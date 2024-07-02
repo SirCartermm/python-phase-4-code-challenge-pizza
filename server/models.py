f74b3facfe9bf5d108c79d5f3103cadfc3adb7ef
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-02 18:45:35 +0300

    modified models.py

--- server/models.py
+++ server/models.py
@@ -5,3 +5,10 @@ db = SQLAlchemy()
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy=True, cascade='all, delete')
+
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+   

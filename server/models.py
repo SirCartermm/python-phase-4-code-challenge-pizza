cf78b13531b43b64dded9cf80cd230eb944adc78
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-02 18:46:32 +0300

    modified models.py

--- server/models.py
+++ server/models.py
@@ -14,3 +14,10 @@ class Pizza(db.Model):
     ingredients = db.Column(db.String(200), nullable=False)
     restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', lazy=True, cascade='all, delete')
 
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant = db.relationship('Restaurant', backref=db.backref('restaurant_pizzas', lazy=True, cascade='all, delete'))
+    pizza = db.relationship('Pizza', backref=db.backref('restaurant_pizzas', lazy=True, cascade='all, delete'))
